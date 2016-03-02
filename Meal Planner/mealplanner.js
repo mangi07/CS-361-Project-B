a4f4b2437820a0622e20a22c3a00645504c23cc3
Sean Mulholland <smulholl@oregonstate.edu>
2016-03-02 23:49:03 +0000

    mealplanner.js basically working

--- Meal Planner/mealplanner.js
+++ Meal Planner/mealplanner.js
@@ -2,35 +2,61 @@
 var req = new XMLHttpRequest();
 
 var reqData = {};
-/*  Get reqData parts here
-reqData['maxcalories'] = 
-reqData['maxcarbs'] = 
-reqData['maxfat'] = 
-reqData['maxprotein'] = 
-reqData['mincalories'] = 
-reqData['minCarbs'] = 
-reqData['minfat'] = 
-reqData['minprotein'] = 
-*/
+/*  Get reqData parts here */
+reqData['maxcalories'] = "1500";
+reqData['maxcarbs'] = "100";
+reqData['maxfat'] = "100";
+reqData['maxprotein'] = "100";
+reqData['mincalories'] = "0";
+reqData['minCarbs'] = "0";
+reqData['minfat'] = "0";
+reqData['minProtein'] = "0";
+
 
 
 //Build Query Here
+/* Documentation at:
+     https://market.mashape.com/spoonacular/recipe-food-nutrition#find-by-nutrients */
+/* https://spoonacular-recipe-food-nutrition-v1.p.mashape.com */
 var reqQuery = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByNutrients?";
 var reqDataKeys = Object.keys(reqData);
-for(var i = 0; i < reqData.length; i++){
-    reqQuery += reqDataKeys[i] + "=" + reqData[i];
+for(var i = 0; i < reqDataKeys.length; i++){
+    reqQuery += reqDataKeys[i] + "=" + reqData[reqDataKeys[i]];
     if(i != reqData.length -1 ){
         reqQuery += "&";
     }
 }
 
-//Open and send query ***NEEDS API USER/KEY
-req.open("GET", reqQuery , true /*user, password*/);
-req.addEventListener('load', function(){
+var user = "ben2016";
+/*
+   Production Key: bOmaZvaeU8mshuqpe8f0WkZqUCGMp1mxhsnjsnDvVjriaCBS6D 
+   Testing Key: DW9XSMsmJ9mshOb8Nu0OUsVY9ry7p1jwSaSjsnB20ChBTkFVg1
+*/
+var key = "DW9XSMsmJ9mshOb8Nu0OUsVY9ry7p1jwSaSjsnB20ChBTkFVg1";
+//reqQuery += "&X-Mashape-Key=" + key;
+
+var reqCount = 0;
+var failure = 0;
+//Open and send query
+req.open("GET", reqQuery , true);
+req.setRequestHeader("X-Mashape-Key", key);
+req.addEventListener('load', function()
+{
     /**
-     * 
      * Do something here with the req.responseText
-     * 
      */
+     if (req.status >= 500)
+     {
+        //Ensure only 8 requests are made at most
+        if(++reqCount >= 8){
+            failure = 1;
+            console.log("Sorry, we couldn't get a recipe.\n");
+            return;
+        }
+        req.open("GET", reqQuery , true);
+        req.setRequestHeader("X-Mashape-Key", key);
+        req.send();
+     }
+     console.log(req.responseText);
 });
 req.send();
